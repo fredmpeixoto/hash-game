46e1c5d82e2602ee3e0684271183b1b876e4beca
Fred Peixoto <fred.peixoto@softwillians.com.br>
2020-12-10 15:48:14 -0300

    Create game hash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,84 @@
 import { Component } from '@angular/core';
+import { Player } from './shared/player';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hashGame';
+
+  public hashSquad: Player[][];
+  private numberOfMove: number;
+  public currentlyPlayer: string;
+  public currentlyWinner: string;
+
+  constructor() {
+
+    this.currentlyPlayer = 'X';
+    this.numberOfMove = 0;
+
+    this.hashSquad = [
+      [{}, {}, {}],
+      [{}, {}, {}],
+      [{}, {}, {}],
+    ];
+  }
+
+  move(move: Player, indexColumn: number, indexRow: number): void {
+
+    if (move.clicked || this.currentlyWinner) return;
+
+    this.numberOfMove++;
+
+    move.clicked = !move.clicked;
+
+    move.choice = this.currentlyPlayer;
+
+    if (this.numberOfMove > 4 && this.checkWinner(move.choice, indexColumn, indexRow)) {
+      this.currentlyWinner = move.choice;
+      return;
+    }
+
+    this.currentlyPlayer = this.currentlyPlayer === 'X' ? 'O' : 'X';
+  }
+
+  checkWinner(myChoice: string, indexColumn: number, indexRow: number): boolean {
+    return this.thereIsSameChoiceByRow(myChoice, indexRow) ||
+      this.thereIsSameChoiceByColumn(myChoice, indexColumn) ||
+      this.thereIsByXChoice(myChoice);
+  }
+
+  thereIsSameChoiceByRow(myChoice: string, indexRow: number): boolean {
+
+    return this.hashSquad[indexRow].every(column => column.choice === myChoice);
+
+  }
+
+  thereIsSameChoiceByColumn(myChoice: string, indexColumn: number): boolean {
+
+    return this.hashSquad.every(row => row[indexColumn].choice === myChoice);
+
+  }
+
+  thereIsByXChoice(myChoice: string): boolean {
+
+    const leftToRight = this.hashSquad.every((row, indexRow) => row[indexRow].choice === myChoice);
+
+
+    const rightToLeft = this.hashSquad.every((row, indexRow) => this.isSomeChoiceDecreasing(indexRow, row, myChoice));
+
+    return (leftToRight || rightToLeft);
+
+  }
+
+  private isSomeChoiceDecreasing(indexRow: number, row: Player[], myChoice: string): boolean {
+
+    const arrayLenght = (this.hashSquad.length - 1);
+
+    const indexSearch = arrayLenght - indexRow;
+
+    return row[indexSearch].choice === myChoice;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
+    CommonModule,
     BrowserModule
   ],
   providers: [],
